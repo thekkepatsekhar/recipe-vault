bf735a3add94b8e234dbb8a5cc8b8662e13badfc
thekkepatsekhar <thekkepatsekhar@gmail.com>
2026-08-05 09:38:29 -0400

    Add onRequest function to handle requests

---
+++ functions/config.js
@@ -0,0 +1,17 @@
+export async function onRequest(context) {
+  const js = `(function() {
+  const defaults = {
+    rv_anthropic_key: '${context.env.ANTHROPIC_KEY}',
+    rv_youtube_key: '${context.env.YOUTUBE_KEY}',
+  };
+  Object.entries(defaults).forEach(([key, value]) => {
+    if (value && !localStorage.getItem(key)) {
+      localStorage.setItem(key, value);
+    }
+  });
+})();`;
+
+  return new Response(js, {
+    headers: { 'Content-Type': 'application/javascript' },
+  });
+}
