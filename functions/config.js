4dbf51f7522530ae2fd2d30d4551a7e3a2650e4c
thekkepatsekhar <thekkepatsekhar@gmail.com>
2026-08-05 11:45:23 -0400

    Fix formatting and closure in onRequest function
    Fix formatting of the rv_youtube_key line and ensure proper closure of the function.

--- functions/config.js
+++ functions/config.js
@@ -2,7 +2,8 @@ export async function onRequest(context) {
   const js = `(function() {
   const defaults = {
     rv_anthropic_key: '${context.env.ANTHROPIC_KEY}',
-    rv_youtube_key: '${context.env.YOUTUBE_KEY}',
+    rv_youtube_key:   '${context.env.YOUTUBE_KEY}',
+    rv_deepseek_key:  '${context.env.DEEPSEEK_KEY}',
   };
   Object.entries(defaults).forEach(([key, value]) => {
     if (value && !localStorage.getItem(key)) {
