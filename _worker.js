02906add1f5ee3a6959739d663787ad86121b4aa
thekkepatsekhar <thekkepatsekhar@gmail.com>
2026-09-03 14:48:16 -0400

    Refactor JavaScript response generation in _worker.js

--- _worker.js
+++ _worker.js
@@ -5,9 +5,7 @@ export default {
     if (url.pathname === '/js/config.js') {
       const js = `(function() {
   const defaults = {
-    rv_anthropic_key: '${env.ANTHROPIC_API_KEY}',
-    rv_youtube_key:   '${env['YouTube API Key']}',
-    rv_deepseek_key:  '${env.DEEPSEEK_KEY}',
+    rv_deepseek_key: '${env.DEEPSEEK_KEY}',
   };
   Object.entries(defaults).forEach(([key, value]) => {
     if (value && !localStorage.getItem(key)) {
@@ -15,12 +13,8 @@ export default {
     }
   });
 })();`;
-
       return new Response(js, {
-        headers: {
-          'Content-Type': 'application/javascript',
-          'Cache-Control': 'no-store',
-        },
+        headers: { 'Content-Type': 'application/javascript', 'Cache-Control': 'no-store' },
       });
     }
 
